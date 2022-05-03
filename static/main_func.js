b674592bdf2a5ac292c1b721dc471840f2edfe58
sunset <104331869+minkkky@users.noreply.github.com>
2022-05-03 17:00:33 +0900

    파일 업로드

---
+++ static/main_func.js
@@ -0,0 +1,23 @@
+// ... 버튼 모달창 열기
+function popOpen() {
+    var modalPop = $('.modal-wrap');
+    var modalBg = $('.modal-bg');
+    $(modalPop).show();
+    $(modalBg).show();
+
+    $('html').css({
+        overflow: 'hidden',
+        height: 'auto'
+    });
+}
+
+// ... 버튼 모달창 닫기
+function popClose() {
+    var modalPop = $('.modal-wrap');
+    var modalBg = $('.modal-bg');
+
+    $(modalPop).hide();
+    $(modalBg).hide();
+
+    $('html').removeAttr('style');
+}
