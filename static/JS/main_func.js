9dbaa6623f52b132097c6a826fb4d7a036a56a9d
zeonga1102 <dlwjddk4587@naver.com>
2022-05-04 15:31:22 +0900

    사용자 로컬 이미지 파일 선택 기능 추가

--- static/JS/main_func.js
+++ static/JS/main_func.js
@@ -1,7 +1,7 @@
 // ... 버튼 모달창 열기
 function popOpen() {
     var modalPop = $('.modal-wrap');
-    var modalBg = $('.modal-bg');
+    var modalBg = $('#modal');
     $(modalPop).show();
     $(modalBg).show();
 
@@ -20,4 +20,60 @@ function popClose() {
     $(modalBg).hide();
 
     $('html').removeAttr('style');
+}
+
+
+function selectImages(event) {
+    $('.modal-content').empty();
+    let sel_files = [];
+    let files = event.target.files;
+    let fileArr = Array.prototype.slice.call(files);
+    let index = 0;
+
+    let carousel = `<div id="carousel_upload" class="carousel slide" data-bs-ride="carousel" data-bs-interval="false">
+                          <div id="carousel_upload_inner" class="carousel-inner">
+<!--                            <div class="carousel-item active">-->
+<!--                              <img src="..." class="d-block w-100" alt="...">-->
+<!--                            </div>-->
+<!--                            <div class="carousel-item">-->
+<!--                              <img src="..." class="d-block w-100" alt="...">-->
+<!--                            </div>-->
+<!--                            <div class="carousel-item">-->
+<!--                              <img src="..." class="d-block w-100" alt="...">-->
+<!--                            </div>-->
+                          </div>
+                          <button class="carousel-control-prev" type="button" data-bs-target="#carousel_upload" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                          </button>
+                          <button class="carousel-control-next" type="button" data-bs-target="#carousel_upload" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                          </button>
+                        </div>`
+
+    $('.modal-content').append(carousel)
+
+    fileArr.forEach(function (f) {
+        sel_files.push(f);
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            // let temp = `<img src=${e.target.result} data-file=${f.name}/>`
+            let temp = ``
+            if(index == 0) {
+                temp = `<div class="carousel-item active">
+                              <img src=${e.target.result} class="d-block w-100" alt="...">
+                            </div>`
+                index++;
+            }
+            else {
+                temp = `<div class="carousel-item">
+                              <img src=${e.target.result} class="d-block w-100" alt="...">
+                            </div>`
+            }
+
+            $('#carousel_upload_inner').append(temp);
+        };
+        reader.readAsDataURL(f);
+    })
 }
