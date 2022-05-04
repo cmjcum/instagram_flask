40740ec0d0ac882d20b8c9fe8840c0795f21048c
LULULALA2 <lhg9714@gmail.com>
2022-05-04 09:49:02 +0900

    폴더 추가 및 경로 수정

---
+++ static/JS/main_func.js
@@ -0,0 +1,25 @@
+// ... 버튼 모달창 열기
+function popOpen() {
+    var modalPop = $('.modal-wrap');
+    var modalBg = $('.modal-bg');
+    $(modalPop).show();
+    $(modalBg).show();
+
+    $('html').css({
+        overflow: 'hidden',
+        height: 'auto'
+    });
+}
+
+// ... 버튼 모달창 닫기
+function popClose() {
+    var modalPop = $('.modal-wrap');
+    var modalBg = $('.modal-bg');
+
+    $(modalPop).hide();
+    $(modalBg).hide();
+
+    $('html').removeAttr('style');
+}
+
+// ... 버튼 모달창 닫기
