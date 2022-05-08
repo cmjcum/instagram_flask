0cfbe25fbc5f6179d96dd507e3baa58be1fbd07f
m_ink <mp98dl4@gmail.com>
2022-05-09 01:02:18 +0900

    프로필 페이지 팔로우, 팔로워 리스트 모달창 추가 / 게시글 영역 코드 수정

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
-#Flask 객체 인스턴스 생성
+from pymongo import MongoClient
+
+
+# Flask 객체 인스턴스 생성
 app = Flask(__name__)
 
-@app.route('/') # 접속하는 url
+
+@app.route('/')  # 접속하는 url
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/profile') # 접속하는 url
+@app.route('/profile')  # 접속하는 url
 def profile():
-  return render_template('profile.html')
+    return render_template('profile.html')
 
-if __name__=="__main__":
-  app.run(debug=True)
-  # host 등을 직접 지정하고 싶다면
-  # app.run(host="127.0.0.1", port="5000", debug=True)
+
+@app.route("/profile", methods=["POST"])
+def bucket_post():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': 'POST(기록) 연결 완료!'})
+
+
+@app.route("/profile", methods=["GET"])
+def follow_get():
+    # 팔로잉, 팔로워 db에서 내가 들어간 것들 가져오기
+
+
+def thumbnail_get():
+    # 유저 db에서 최근 게시물 9개 사진 가져오기
+    return jsonify({'msg': 'GET 연결 완료!'})
+
+
+if __name__ == "__main__":
+    # app.run(debug=True)
+    # host 등을 직접 지정하고 싶다면
+    app.run(host="0.0.0.0", port="5000", debug=True)
--- static/JS/profile_func.js
+++ static/JS/profile_func.js
@@ -1,6 +1,21 @@
-// ... 버튼 모달창 열기
+// $(document).ready(function () {
+//     show_follow();
+// });
+
+function show_follow() {
+    $.ajax({
+        type: "GET",
+        url: "/profile",
+        data: {},
+        success: function (response) {
+            alert(response["msg"])
+        }
+    });
+}
+
+// 톱니바퀴 모달창 열기
 function popOpen() {
-    var modalPop = $('.modal-wrap');
+    var modalPop = $('#modal-gear');
     var modalBg = $('.modal-bg');
     $(modalPop).show();
     $(modalBg).show();
@@ -10,8 +25,31 @@ function popOpen() {
         height: 'auto'
     });
 }
+// 팔로우 모달창 열기
+function popFollowingOpen() {
+    var modalPop = $('#modal-following');
+    var modalBg = $('.modal-bg');
+    $(modalPop).show();
+    $(modalBg).show();
 
-// ... 버튼 모달창 닫기
+    $('html').css({
+        overflow: 'hidden',
+        height: 'auto'
+    });
+}
+//팔로워 모달창 열기
+function popFollowerOpen() {
+    var modalPop = $('#modal-follower');
+    var modalBg = $('.modal-bg');
+    $(modalPop).show();
+    $(modalBg).show();
+
+    $('html').css({
+        overflow: 'hidden',
+        height: 'auto'
+    });
+}
+// 모달창 닫기
 function popClose() {
     var modalPop = $('.modal-wrap');
     var modalBg = $('.modal-bg');
