3edec35d8c71f59dc3e23e46e4cf27a7e8ce3bb3
yinmsk <galaxy220111@gmail.com>
2022-05-09 11:47:37 +0900

    회원가입 정보 db 추가

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
-from flask import Flask, render_template
-
-#Flask 객체 인스턴스 생성
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-@app.route('/') # 접속하는 url
-def index():
-  return render_template('index.html')
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.gsb7w.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbSignupPrac
 
-if __name__=="__main__":
-  app.run(debug=True)
-  # host 등을 직접 지정하고 싶다면
-  # app.run(host="127.0.0.1", port="5000", debug=True)
+
+
+
+@app.route('/')
+def home():
+   return render_template('sign_up.html')
+
+
+
+@app.route('/signupPrac', methods=['POST'])
+def sign_up_post():
+   email_receive = request.form['email_give']
+   id_receive = request.form['id_give']
+   name_receive = request.form['name_give']
+   password_receive = request.form['password_give']
+
+   doc = {
+      'email': email_receive,
+      'id': id_receive,
+      'name': name_receive,
+      'password': password_receive
+   }
+   db.signupPrac.insert_one(doc)
+
+   return jsonify({'msg': '가입완료'})
+
+
+@app.route('/signupPrac', methods=['GET'])
+def sing_up_get():
+   return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+
+
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
