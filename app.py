2e500e4c23db728941adf7dab26ddbf88904d1b5
zeonga1102 <dlwjddk4587@naver.com>
2022-05-10 19:52:38 +0900

    CLN

--- app.py
+++ app.py
@@ -15,97 +15,6 @@ db = client.instaClone
 # Flask 객체 인스턴스 생성
 app = Flask(__name__)
 
-@app.route('/api/feed', methods=["POST"])
-def upload():
-    images = request.files.getlist("image[]")
-    desc = request.form['desc']
-    location = request.form['location']
-    time = datetime.now()
-    email = "LULULALA_2@insta.com"
-    user_id = "LULULALA_2"
-    images_path = []
-
-    for image in images:
-        timestamp = datetime.now().strftime('%Y%m%d%H%M%S%f')
-        path = './static/img/post_img/' + timestamp + secure_filename(image.filename)
-        print(path)
-        image.save(path)
-        images_path.append(path)
-
-    doc = {
-        'email': email,
-        'user_id': user_id,
-        'photo': images_path,
-        'location': location,
-        'post_date': time,
-        'desc': desc
-    }
-
-    db.posts.insert_one(doc)
-
-    return jsonify({'msg': '완료!'})
-
-
-@app.route('/feed', methods=['GET'])
-def getFeed():
-  # 피드 조회하기
-  posts = list(db.posts.find({}).sort('post_date', -1).limit(20))
-
-  for post in posts:
-    post["_id"] = str(post["_id"])
-
-  return jsonify({'posts': posts, 'result': 'success'})
-
-
-@app.route('/api/comment', methods=['GET'])
-def getComment():
-  # 댓글 조회하기
-  post_id = request.args['post_id_give']
-  all_comments = list(db.comments.find({'post_id': post_id}))
-
-  for comment in all_comments:
-    comment["_id"] = str(comment["_id"])
-    print(comment['email'])
-    user_info = db.users.find_one({"email": comment['email']})
-    print(user_info['user_id'])
-    comment['user_id'] = user_info['user_id']
-
-  return jsonify({'comments': all_comments})
-
-
-@app.route('/api/comment', methods=['POST'])
-def postComment():
-  # 댓글 작성하기
-  # user_info = db.users.find_one({"email": 'LULULALA_2@insta.com'})
-  post_id_receive = request.form['post_id_give']
-  comment_receive = request.form['comment_give']
-  cmt_date_receive = request.form['cmt_date_give']
-
-  doc = {'post_id': post_id_receive, 'email': 'LULULALA_2@insta.com', 'comment': comment_receive, 'cmt_date': cmt_date_receive}
-  db.comments.insert_one(doc)
-
-  return jsonify({'msg': '댓글 작성 완료!'})
-
-
-# @app.route('/api/like', methods=['POST'])
-# def updateLike():
-#   # 좋아요 업데이트
-#
-#   user_info = db.users.find_one({'email': 'LULUALA_2@insta.com'})
-#   post_id_receive = request.form['post_id_give']
-#   action_receive = request.form['action_give']
-#
-#   doc = {'post_id': post_id_receive,
-#          'user_id': user_info['user_id']}
-#
-#   if action_receive == "like":
-#     db.likes.insert_one(doc)
-#   else:
-#     db.likes.delete_one(doc)
-#
-#   count = db.likes.count_documents({"post_id": post_id_receive})
-#   return jsonify({'result': 'success', 'msg': 'updated', 'count':count})
-
 SECRET_KEY = 'CMG'
 
 @app.route('/')
@@ -126,7 +35,6 @@ def home():
    except jwt.exceptions.DecodeError:
       return render_template('login_page.html')
       # return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다"))
-
 
 @app.route('/signup')
 def home_signup():
@@ -193,33 +101,92 @@ def sing_up_get():
 
    # return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
 
+@app.route('/api/feed', methods=["POST"])
+def upload():
+    images = request.files.getlist("image[]")
+    desc = request.form['desc']
+    location = request.form['location']
+    time = datetime.now()
+    email = "LULULALA_2@insta.com"
+    user_id = "LULULALA_2"
+    images_path = []
 
-@app.route('/profile')  # 접속하는 url
-def profile():
-    return render_template('profile.html')
+    for image in images:
+        timestamp = datetime.now().strftime('%Y%m%d%H%M%S%f')
+        path = './static/img/post_img/' + timestamp + secure_filename(image.filename)
+        print(path)
+        image.save(path)
+        images_path.append(path)
 
+    doc = {
+        'email': email,
+        'user_id': user_id,
+        'photo': images_path,
+        'location': location,
+        'post_date': time,
+        'desc': desc
+    }
 
-@app.route('/<user_id>')
-def user(user_id):
-    email = 'LULULALA_2@insta.com'  # 나중에 토큰에서 메일주소 받으면 되겠지?
-    user_info = db.users.find_one({"email": email}, {"_id": False})
+    db.posts.insert_one(doc)
 
-    user_id = user_info['user_id']
-    user_pic = user_info['pic']
-    user_bio = user_info['bio']
-    user_name = user_info['name']
+    return jsonify({'msg': '완료!'})
 
-    feed_cnt = db.posts.count_documents({"email": email})
-    follower_cnt = db.follow.count_documents({"t_email": email})
-    following_cnt = db.follow.count_documents({"email": email})
+@app.route('/feed', methods=['GET'])
+def getFeed():
+  # 피드 조회하기
+  posts = list(db.posts.find({}).sort('post_date', -1).limit(20))
 
-    follower_data = db.follow.find({"t_email": email})
-    following_data = db.follow.find({"email": email})
+  for post in posts:
+    post["_id"] = str(post["_id"])
 
+  return jsonify({'posts': posts, 'result': 'success'})
 
-    return render_template('profile.html', user_id=user_id, user_pic=user_pic, user_bio=user_bio, user_name=user_name,
-                           follower_cnt=follower_cnt, following_cnt=following_cnt, feed_cnt=feed_cnt, follower_data=follower_data, following_data=following_data)
+@app.route('/api/comment', methods=['GET'])
+def getComment():
+  # 댓글 조회하기
+  post_id = request.args['post_id_give']
+  all_comments = list(db.comments.find({'post_id': post_id}))
 
+  for comment in all_comments:
+    comment["_id"] = str(comment["_id"])
+    print(comment['email'])
+    user_info = db.users.find_one({"email": comment['email']})
+    print(user_info['user_id'])
+    comment['user_id'] = user_info['user_id']
+
+  return jsonify({'comments': all_comments})
+
+@app.route('/api/comment', methods=['POST'])
+def postComment():
+  # 댓글 작성하기
+  # user_info = db.users.find_one({"email": 'LULULALA_2@insta.com'})
+  post_id_receive = request.form['post_id_give']
+  comment_receive = request.form['comment_give']
+  cmt_date_receive = request.form['cmt_date_give']
+
+  doc = {'post_id': post_id_receive, 'email': 'LULULALA_2@insta.com', 'comment': comment_receive, 'cmt_date': cmt_date_receive}
+  db.comments.insert_one(doc)
+
+  return jsonify({'msg': '댓글 작성 완료!'})
+
+# @app.route('/api/like', methods=['POST'])
+# def updateLike():
+#   # 좋아요 업데이트
+#
+#   user_info = db.users.find_one({'email': 'LULUALA_2@insta.com'})
+#   post_id_receive = request.form['post_id_give']
+#   action_receive = request.form['action_give']
+#
+#   doc = {'post_id': post_id_receive,
+#          'user_id': user_info['user_id']}
+#
+#   if action_receive == "like":
+#     db.likes.insert_one(doc)
+#   else:
+#     db.likes.delete_one(doc)
+#
+#   count = db.likes.count_documents({"post_id": post_id_receive})
+#   return jsonify({'result': 'success', 'msg': 'updated', 'count':count})
 
 # @app.route('/user/<username>')
 # def user(username):
@@ -284,7 +251,30 @@ def modifyFeed():
 
     return jsonify({'msg': '수정 완료!'})
 
+@app.route('/profile')  # 접속하는 url
+def profile():
+    return render_template('profile.html')
 
+@app.route('/<user_id>')
+def user(user_id):
+    email = 'LULULALA_2@insta.com'  # 나중에 토큰에서 메일주소 받으면 되겠지?
+    user_info = db.users.find_one({"email": email}, {"_id": False})
+
+    user_id = user_info['user_id']
+    user_pic = user_info['pic']
+    user_bio = user_info['bio']
+    user_name = user_info['name']
+
+    feed_cnt = db.posts.count_documents({"email": email})
+    follower_cnt = db.follow.count_documents({"t_email": email})
+    following_cnt = db.follow.count_documents({"email": email})
+
+    follower_data = db.follow.find({"t_email": email})
+    following_data = db.follow.find({"email": email})
+
+
+    return render_template('profile.html', user_id=user_id, user_pic=user_pic, user_bio=user_bio, user_name=user_name,
+                           follower_cnt=follower_cnt, following_cnt=following_cnt, feed_cnt=feed_cnt, follower_data=follower_data, following_data=following_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
