5b6847d144bf3f02a9e49b562702ac19e4298de3
zeonga1102 <dlwjddk4587@naver.com>
2022-05-11 06:46:05 +0900

    프로필 페이지에서 포스팅 모달 추가

--- static/JS/profile_func.js
+++ static/JS/profile_func.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     getFeed()
     getFollower()
     getFollowing()
+    showModalProfilePic()
 })
 
 // ... 피드 불러오기
@@ -187,4 +188,127 @@ function goHome() {
 
 function goUser() {
     window.location.href = "/user";
+}
+
+// 포스팅 모달 함수
+function selectImages(event) {
+    $.ajax({
+        type: 'GET',
+        url: '/api/userInfo',
+        data: {},
+        success: function (response) {
+            let user_info = response['user_info'];
+            let user_id = user_info['user_id']
+            $('#modal_post_id').text(user_id)
+        }
+    });
+
+    $('#modal_post_display_image').empty();
+    $('#image_upload_button').hide();
+    let sel_files = [];
+    let files = event.target.files;
+    let fileArr = Array.prototype.slice.call(files);
+    let index = 0;
+
+    let temp_img_html = ``
+
+    if(fileArr.length == 1) {
+        temp_img_html = `<div id="carousel_upload" class="carousel slide" data-bs-ride="carousel" data-bs-interval="false">
+                          <div id="carousel_upload_inner" class="carousel-inner"></div></div>`
+    }
+    else {
+        temp_img_html = `<div id="carousel_upload" class="carousel slide" data-bs-ride="carousel" data-bs-interval="false">
+                          <div id="carousel_upload_inner" class="carousel-inner">
+                          </div>
+                          <button class="carousel-control-prev" type="button" data-bs-target="#carousel_upload" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                          </button>
+                          <button class="carousel-control-next" type="button" data-bs-target="#carousel_upload" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                          </button>
+                        </div>`
+    }
+
+    $('#modal_post_display_image').append(temp_img_html)
+
+    fileArr.forEach(function (f) {
+        sel_files.push(f);
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            let temp = ``
+            if(index == 0) {
+                temp = `<div class="carousel-item active">
+                              <img src=${e.target.result} class="d-block w-100" alt="...">
+                            </div>`
+                index++;
+            }
+            else {
+                temp = `<div class="carousel-item">
+                              <img src=${e.target.result} class="d-block w-100" alt="...">
+                            </div>`
+            }
+
+            $('#carousel_upload_inner').append(temp);
+        };
+        reader.readAsDataURL(f);
+    })
+    $('#modal_post_text').show();
+    $('#button_post').show();
+}
+
+function postFeed() {
+    let form = $('#post_form')[0];
+    let formData = new FormData(form);
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/feed',
+        data: formData,
+        // enctype: 'multipart/form-data',
+        cache: false,
+        processData: false,
+        contentType: false,
+        success: function (response) {
+            alert(response['msg']);
+            $("#modal_post").removeClass("is-active");
+            window.location.reload();
+        },
+        error: function (e) {
+            console.log("ERROR: ", e);
+            alert("fail");
+            $("#modal_post").removeClass("is-active");
+            window.location.reload();
+        }
+    });
+}
+
+function initPostModal() {
+    console.log("init")
+    $('#modal_post').addClass('is-active');
+    $('#modal_post_display_image').empty();
+    $('#image_upload_button').show();
+
+    $('#modal_post_text').hide();
+    $('#button_post').hide();
+}
+
+// 모달에 사용자 프로필 사진 띄우는 함수
+function showModalProfilePic() {
+    $.ajax({
+        type: 'GET',
+        url: '/api/userInfo',
+        data: {},
+        success: function (response) {
+            let user_info = response['user_info'];
+            let pic = user_info['pic']
+            if(pic == null) {
+                $('.modal_profile_pic').attr("src", '../static/img/profile.jpg');
+            }
+            else {
+               $('.modal_profile_pic').attr("src",pic);
+            }
+        }
+    });
 }
