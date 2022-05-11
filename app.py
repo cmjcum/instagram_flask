3011961af3c576dba6816ed892b6b2997838e382
LULULALA2 <lhg9714@gmail.com>
2022-05-11 12:13:28 +0900

    CLN

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def api_login():
     if result is not None:
        payload = {
          'id': id_receive,
-         'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=100)
+         'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=10000)
        }
        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
